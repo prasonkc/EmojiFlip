fcb040ab7156394e3fde7dbfc45c96a5bb6eee69
Prason <prasonkc07@gmail.com>
2025-08-14 21:37:25 +0545

    Edited UI Layout

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <h1 className="text-center">Flip Your Cards!!</h1>
+
+      <div className="h-screen bg-gray-900 flex items-center justify-center">
+
+        <div className="container bg-gray-800  flex m-auto h-[90vh] rounded-lg items-center justify-start p-10 gap-5">
+
+        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
+        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
+        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
