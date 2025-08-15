7e385f342959f8892f835a127d091542ec395324
Prason <prasonkc07@gmail.com>
2025-08-15 09:05:03 +0545

    Added images and shuffled them using Fisher-Yates algorithm

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,11 +4,27 @@ import "./App.css";
 const SIZE = 24; //Size should always be an even number
 function Card() {
   return (
-    <div className="card size-50 bg-gray-700 rounded-3xl m-3">
+    <div className="card size-50 bg-gray-700 rounded-3xl m-3 shadow-2xl hover: cursor-pointer transform hover:scale-105 transition-transform duration-200 ease-in-out">
       <img src="" alt="" />
     </div>
   );
 }
+
+const IMAGES = [
+  "https://www.flaticon.com/free-stickers/emoticon",
+  "https://www.flaticon.com/free-stickers/bear",
+  "https://www.flaticon.com/free-stickers/funny",
+  "https://www.flaticon.com/free-stickers/duck",
+  "https://www.flaticon.com/free-stickers/bird",
+  "https://www.flaticon.com/free-stickers/bear",
+  "https://www.flaticon.com/free-stickers/duck",
+  "https://www.flaticon.com/free-stickers/love",
+  "https://www.flaticon.com/free-stickers/death",
+  "https://www.flaticon.com/free-stickers/bear",
+  "https://www.flaticon.com/free-stickers/bear",
+  "https://www.flaticon.com/free-stickers/bunny",
+  "https://www.flaticon.com/free-stickers/lion",
+];
 
 function Container() {
   const cards = [];
@@ -22,6 +38,26 @@ function Container() {
   );
 }
 
+function generateCardImages(SIZE) {
+  let selectedImages = [];
+
+  for (let i = 0; i < SIZE / 2; i++) {
+    let img = IMAGES[i % IMAGES.length];
+    selectedImages.push(img, img); // Push each image twice for pairs
+  }
+
+  //Fisher-Yates shuffle algorithm
+  for (let i = selectedImages.length - 1; i > 0; i--) {
+    //loop from the end to beginning
+    const j = Math.floor(Math.random() * (i + 1)); //Pick a random index
+    [selectedImages[i], selectedImages[j]] = [
+      selectedImages[j],
+      selectedImages[i],
+    ]; //swap the elements
+  }
+  return selectedImages;
+}
+
 function App() {
   return (
     <>
@@ -30,7 +66,7 @@ function App() {
       <div className="h-screen bg-gray-900 flex items-center justify-center">
         <Container />
       </div>
-    </> 
+    </>
   );
 }
 
