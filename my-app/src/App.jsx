75285cb1127467f1d03029374e68aeff9a5b0a51
Prason <prasonkc07@gmail.com>
2025-08-15 07:55:26 +0545

    Container margin and padding fixed

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import "./App.css";
 
+const SIZE = 24; //Size should always be an even number
+function Card() {
+  return (
+    <div className="card size-50 bg-gray-700 rounded-3xl m-3">
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+function Container() {
+  const cards = [];
+  for (let i = 0; i < SIZE; i++) {
+    cards.push(<Card />);
+  }
+  return (
+    <div className="container bg-gray-800 flex m-auto rounded-2xl items-center justify-center py-5 flex-wrap w-fit">
+      {cards}
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
       <h1 className="text-center">Flip Your Cards!!</h1>
 
       <div className="h-screen bg-gray-900 flex items-center justify-center">
-
-        <div className="container bg-gray-800  flex m-auto h-[90vh] rounded-lg items-center justify-start p-10 gap-5">
-
-        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
-        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
-        <div className="card size-50 bg-gray-700 rounded-3xl sh"></div>
+        <Container />
       </div>
-      </div>
-    </>
+    </> 
   );
 }
 
