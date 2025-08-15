09b0040c5b20be03a29089c9f287d39394b78d4c
Prason <prasonkc07@gmail.com>
2025-08-15 10:12:41 +0545

    statically loaded the stickers

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,39 +1,64 @@
 import { useState } from "react";
 import "./App.css";
+import sticker1 from "../assets/fischl_1.jpg";
+import sticker2 from "../assets/furina_1.jpg";
+import sticker3 from "../assets/hu_tao_1.jpg";
+import sticker4 from "../assets/keqing_1.jpg";
+import sticker5 from "../assets/klee_1.jpg";
+import sticker6 from "../assets/klee_2.jpg";
+import sticker7 from "../assets/nahida_1.jpg";
+import sticker8 from "../assets/nahida_2.jpg";
+import sticker9 from "../assets/navia_1.jpg";
+import sticker10 from "../assets/navia_2.jpg";
+import sticker11 from "../assets/yoimiya_1.jpg";
+import sticker12 from "../assets/paimon_2.jpg";
+import sticker13 from "../assets/yoimiya_2.jpg";
+import sticker14 from "../assets/yoimiya_3.jpg";
+import sticker15 from "../assets/yae_1.jpg";
+import sticker16 from "../assets/shogun_1.jpg";
+import sticker17 from "../assets/paimon_1.jpg";
 
 const SIZE = 24; //Size should always be an even number
-function Card() {
+function Card({ img, idx }) {
   return (
     <div className="card size-50 bg-gray-700 rounded-3xl m-3 shadow-2xl hover: cursor-pointer transform hover:scale-105 transition-transform duration-200 ease-in-out">
-      <img src="" alt="" />
+      <img src={img} alt={`Card${idx}`} className="w-full h-full object-cover rounded-3xl" />
     </div>
   );
 }
 
 const IMAGES = [
-  "https://www.flaticon.com/free-stickers/emoticon",
-  "https://www.flaticon.com/free-stickers/bear",
-  "https://www.flaticon.com/free-stickers/funny",
-  "https://www.flaticon.com/free-stickers/duck",
-  "https://www.flaticon.com/free-stickers/bird",
-  "https://www.flaticon.com/free-stickers/bear",
-  "https://www.flaticon.com/free-stickers/duck",
-  "https://www.flaticon.com/free-stickers/love",
-  "https://www.flaticon.com/free-stickers/death",
-  "https://www.flaticon.com/free-stickers/bear",
-  "https://www.flaticon.com/free-stickers/bear",
-  "https://www.flaticon.com/free-stickers/bunny",
-  "https://www.flaticon.com/free-stickers/lion",
+  sticker1,
+  sticker2,
+  sticker3,
+  sticker4,
+  sticker5,
+  sticker6,
+  sticker7,
+  sticker8,
+  sticker9,
+  sticker10,
+  sticker11,
+  sticker12,
+  sticker13,
+  sticker14,
+  sticker15,
+  sticker16,
+  sticker17,
 ];
 
 function Container() {
   const cards = [];
+  const images = generateCardImages(SIZE);
   for (let i = 0; i < SIZE; i++) {
     cards.push(<Card />);
   }
   return (
+    
     <div className="container bg-gray-800 grid grid-cols-6 m-auto rounded-2xl items-center justify-center p-5 w-fit">
-      {cards}
+      {images.map((img, idx) => (
+        <Card key={idx} img={img} idx={idx} />
+      ))}
     </div>
   );
 }
